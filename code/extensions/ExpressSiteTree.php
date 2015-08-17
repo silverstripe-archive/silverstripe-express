cb460edaf3f2255c772bf4e380b83099007e7083
Corey Sewell <corey@gdmedia.tv>
2015-08-18 09:29:22 +1200

    Cache menu MenuChildren

--- code/extensions/ExpressSiteTree.php
+++ code/extensions/ExpressSiteTree.php
@@ -2,10 +2,14 @@
 
 class ExpressSiteTree extends SiteTreeExtension {
 
-    static $icon = 'silverstripe-gdm-express/assets/images/sitetree_images/page.png';
+	static $icon = 'silverstripe-gdm-express/assets/images/sitetree_images/page.png';
+	private $menuChildren;
 
-    public function MenuChildren() {
-        return $this->owner->Children()->filter('ShowInMenus', true);
-    }
+	public function MenuChildren() {
+		if (!$this->menuChildren) {
+			$this->menuChildren = $this->owner->Children()->filter('ShowInMenus', true);
+		}
+		return $this->menuChildren;
+	}
 
 }
